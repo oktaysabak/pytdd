5d93c624918a636f91efa34a2866d38c203bb3ba
oktaysabak <oktaysabak@yandex.com>
2020-12-26 00:54:06 +0300

    check virtualenv on bash

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,13 @@
-echo "Discovering tests..."
-coverage run -m unittest discover
-sleep 1
-echo "Reporting tests..."
-coverage report -m --skip-empty
-sleep 1
-echo "Report created. check htmlcov/index.html"
-coverage html --skip-empty
+if [[ $VIRTUAL_ENV != "" ]]
+then
+    echo "Discovering tests..."
+    coverage run -m unittest discover
+    sleep 1
+    echo "Reporting tests..."
+    coverage report -m --skip-empty
+    sleep 1
+    echo "Report created. check htmlcov/index.html"
+    coverage html --skip-empty
+else
+    echo "Activate Virtualenv!"
+fi
